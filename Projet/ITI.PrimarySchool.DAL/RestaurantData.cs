52764626189f69a9b81f49ada1655abab99de1d3
Abdallah rahal <abdalah.rahal15@gmail.com>
2019-10-29 14:04:35 +0100

    intro

---
+++ Projet/ITI.PrimarySchool.DAL/RestaurantData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ITI.PrimarySchool.DAL
+{
+    public class RestaurantData
+    {
+        public int RestaurantId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
