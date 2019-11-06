a8ea8c9a89777c3ea7db2ff0664130c7b1fe5e05
Sami ANKI <sami.anki1316@gmail.com>
2019-11-06 10:08:23 +0100

    with content

--- DealEat/DealEat.DAL/UserData.cs
+++ DealEat/DealEat.DAL/UserData.cs
@@ -6,5 +6,16 @@ namespace DealEat.DAL
 {
     class UserData
     {
+        public int UserId { get; set; }
+        public enum Type { merchant, admin, user }
+
+        //public string Email { get; set; }
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Pseudo { get; set; }
+
+        public byte[] Password { get; set; }
     }
 }
--- DealEat/DealEat.DAL/UserGateway.cs
+++ DealEat/DealEat.DAL/UserGateway.cs
@@ -1,10 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace DealEat.DAL
 {
     class UserGateway
     {
+        readonly string _connectionString;
+
+        public UserGateway(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<UserData> FindById(int userId)
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                return await con.QueryFirstOrDefaultAsync<UserData>(
+                    "select u.UserId, u.Type, u.Name, u.LastName, u.Pseudo, u.[Password] " +
+                    "from dealeat.vUser as u " +
+                    "where u.UserId = @UserId " ,
+                    
+                    new { UserId = userId });
+            }
+        }
+
+        // POUR LE MAIL
+        /*public async Task<UserData> FindByEmail(string email)
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                return await con.QueryFirstOrDefaultAsync<UserData>(
+                    "select u.UserId, u.Type, u.Name, u.LastName, u.Pseudo, u.[Password] " +
+                    "from dealeat.vUser as u " +
+                    "where u.Email = @Email",
+                    new { Email = email });
+            }
+        }*/
+
+        public async Task Delete(int userId)
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                await con.ExecuteAsync("dealeat.sUserDelete", new { UserId = userId }, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+
+
+
+
     }
 }
