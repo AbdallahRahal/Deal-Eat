70d4f0878d71c0bda97d1f0ea96d09d519c14660
Abdallah rahal <abdalah.rahal15@gmail.com>
2019-11-13 09:58:06 +0100

    fgh

--- DealEat/DealEat.DB/Scripts/0001.CreateTable-tUser.sql
+++ DealEat/DealEat.DB/Scripts/0001.CreateTable-tUser.sql
@@ -3,7 +3,7 @@ create table dealeat.tUser
 	UserId int identity(0, 1),
 	[Type] nvarchar(3) collate Latin1_General_100_CI_AS not null,
 	Pseudo nvarchar(45) collate Latin1_General_100_CI_AS not null,
-	[Password] nvarchar(300) collate Latin1_General_100_CI_AS not null,
+	[Password] nvarchar(255) collate Latin1_General_100_CI_AS not null,
 	[Name] nvarchar(45) collate Latin1_General_100_CI_AS not null,
 	LastName nvarchar(45) collate Latin1_General_100_CI_AS not null,
 	Email nvarchar(255) collate Latin1_General_100_CI_AS,
@@ -14,4 +14,4 @@ create table dealeat.tUser
 	constraint PK_tUser primary key(UserId),
 	constraint CK_tUser_Type check([Type] in ('merchant','admin','user'))
 );
-insert into dealeat.tUser([Type], Pseudo, [Password], [Name], LastName) values('user', 'UserPseudoTest', 'aaa', 'UserNameTest', 'UserLastNameTest');
+
